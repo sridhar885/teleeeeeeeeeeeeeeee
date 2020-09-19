e00408aaed5cd8704a75d2d047146dd8ba406013
Roshan Connor <56798243+roshanconnor123@users.noreply.github.com>
2020-09-19 13:46:41 +0530

    Update gc.py

--- gc.py
+++ gc.py
@@ -2,5 +2,5 @@ import sys
 import os
 import getpass
 text_file = open("rclone.conf", "w")
-text_file.write("[GC]\ntype = drive\nscope = drive\nservice_account_file = /telegram_channel_downloader/accounts/1.json\nservice_account_file_path = /telegram_channel_downloader/accounts/")
+text_file.write("[GC]\ntype = drive\nscope = drive\nservice_account_file = accounts/1.json\nservice_account_file_path = accounts/")
 text_file.close()
